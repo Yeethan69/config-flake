c018688c96be5822bbf789f15ea0ea4df12d3d96
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-06-11 20:11:37 +0100

    Allow for not having vars.waybar(.enable)

--- home-manager/common-modules/DEs/bars/waybar.nix
+++ home-manager/common-modules/DEs/bars/waybar.nix
@@ -4,7 +4,7 @@
   ...
 }:
 {
-  config = lib.mkIf (vars.DE.waybar.enable == true) {
+  config = lib.mkIf (lib.hasAttr "waybar" vars.DE && vars.DE.waybar.enable == true) {
     programs.waybar = {
       enable = true;
       systemd.enable = true;
