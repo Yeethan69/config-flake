d7cd0b348f6766e821e1de581471346d4f72b192
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-06-11 20:09:28 +0100

    Maybe fix for sleep on nvidia?

--- nixos/modules/drivers/nvidia.nix
+++ nixos/modules/drivers/nvidia.nix
@@ -10,14 +10,18 @@
     hardware.graphics.enable = true;
 
     services.xserver.videoDrivers = [ "nvidia" ];
+
     boot.initrd.kernelModules = [ "nvidia" ];
     boot.kernelParams = [ "video=DP-2:${vars.display.resolution}@${toString vars.display.refresh}" ];
+
     console.earlySetup = true;
+
     hardware.nvidia = {
       modesetting.enable = true;
       open = true;
       nvidiaSettings = true;
       package = config.boot.kernelPackages.nvidiaPackages.stable;
+      powerManagement.enable = true;
     };
   };
 }
