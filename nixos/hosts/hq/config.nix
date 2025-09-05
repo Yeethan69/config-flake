82a952cca12b0bd468fc3f00cba4a458211352b0
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-09-05 12:45:52 +0100

    Added swapfile and sleep management to hq.

---
+++ nixos/hosts/hq/config.nix
@@ -0,0 +1,17 @@
+{ ... }:
+{
+  swapDevices = [
+    { device = "/swapfile"; }
+  ];
+  
+  boot.kernelParams = [ "resume_offset=12691456" ];
+  boot.resumeDevice = "/dev/disk/by-uuid/2f058807-e05a-4367-944c-ec56757be701";
+
+  powerManagement.enable = true;
+  systemd.sleep.extraConfig = ''
+    AllowSuspend=yes
+    AllowHibernation=yes
+    AllowHybridSleep=no
+    AllowSuspendThenHibernate=no
+  '';
+}
