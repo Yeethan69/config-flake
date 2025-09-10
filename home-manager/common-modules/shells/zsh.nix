7f44f7bd3b0027f637426e175f365255908d8f24
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-09-10 10:06:26 +0100

    Zsh - Add functions for 'nix shell nixpkgs#' and fetcher hashes.

--- home-manager/common-modules/shells/zsh.nix
+++ home-manager/common-modules/shells/zsh.nix
@@ -26,7 +26,38 @@
       initContent = ''
         # Powerlevel10k Zsh theme
         source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-        test -f ~/.config/zsh/.p10k.zsh && source ~/.config/zsh/.p10k.zsh
+
+        if [[ -e ~/.config/zsh/.p10k.zsh ]]; then
+
+          grep -qc custom_nix_shell ~/.config/zsh/.p10k.zsh || ${pkgs.gnused}/bin/sed -i -z '/typeset -g POWERLEVEL9K_LEFT_PROMPT_ELEMENTS=(\n/s/=\n/\n    customnixshell\n/' ~/.config/zsh/.p10k.zsh
+
+          # Show Shell Level
+          function prompt_customnixshell() {
+            if [[ $IN_NIX_SHELL =~ impure ]]; then
+              p10k segment -b 1 -f 3 -i '󰏖'
+            fi
+          }
+
+          source ~/.config/zsh/.p10k.zsh
+        fi
+
+        # Nix shell nixpkgs#... func
+        try() {
+          local args=()
+          for pkg in "$@"; do
+            args+="nixpkgs#$pkg"
+          done
+          IN_NIX_SHELL=impure nix shell "''${args[@]}"
+        }
+
+        # Get hashes for fetchers
+        get-hash() {
+          if [ "$#" -ne 1 ]; then
+            echo "Usage: get-hash <url>"
+            return 1
+          fi
+          nix-prefetch-url "$1" | xargs nix hash convert --hash-algo sha256
+        }
       '';
     };
 
