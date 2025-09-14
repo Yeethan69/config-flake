31c2a8042eae9ca51ff43ab63c791f17aceb8c1e
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-09-14 13:03:49 +0100

    HQ - Enabled fido2 luks unlocking. Made hibernate the default sleep.

--- nixos/hosts/hq/config.nix
+++ nixos/hosts/hq/config.nix
@@ -3,13 +3,18 @@
   swapDevices = [
     { device = "/swapfile"; }
   ];
-  
+
   boot.kernelParams = [ "resume_offset=12691456" ];
   boot.resumeDevice = "/dev/disk/by-uuid/2f058807-e05a-4367-944c-ec56757be701";
+  boot.initrd.systemd.enable = true;
+  boot.initrd.luks.devices."luks-eec9716f-2186-47a2-84f0-bf3f1fc2ca55".crypttabExtraOpts = [
+    "fido2-device=auto"
+    "timeout=60"
+  ];
 
   powerManagement.enable = true;
   systemd.sleep.extraConfig = ''
-    AllowSuspend=yes
+    AllowSuspend=no
     AllowHibernation=yes
     AllowHybridSleep=no
     AllowSuspendThenHibernate=no
