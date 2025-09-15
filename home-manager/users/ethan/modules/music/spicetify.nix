2b784009bdc7836c0fd34d3aa849fdbd798f4ac8
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-09-15 23:08:32 +0100

    Spotify - Change to purple dribbblish theme.

--- home-manager/users/ethan/modules/music/spicetify.nix
+++ home-manager/users/ethan/modules/music/spicetify.nix
@@ -15,7 +15,12 @@ in
         lyricsPlus
         betterLibrary
       ];
-      theme = spicePkgs.themes.blossom;
+      theme = spicePkgs.themes.dribbblish;
+      colorScheme = "purple";
+      enabledSnippets = [
+        # https://github.com/spicetify/spicetify-themes/issues/1159#issuecomment-2705286541
+        ''.encore-dark-theme, .encore-dark-theme .encore-base-set{--background-base:rgba(0,0,0,0);}''
+      ];
     };
   };
 }
