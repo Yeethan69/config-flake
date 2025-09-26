9d3eb7b043186f4791efa32b147c3a5f8fc5fb36
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-09-27 00:37:47 +0100

    Waybar - Moved config completely to common module.

--- home-manager/common-modules/DEs/bars/waybar.nix
+++ home-manager/common-modules/DEs/bars/waybar.nix
@@ -1,6 +1,7 @@
 {
   lib,
   vars,
+  pkgs,
   ...
 }:
 {
@@ -9,6 +10,270 @@
       enable = true;
       systemd.enable = true;
       systemd.target = "graphical-session.target";
+
+      # https://github.com/thnikk/fuzzel-scripts/blob/master/fuzzel-powermenu.sh
+      style = ''
+        * {
+          border: none;
+          border-radius: 5;
+        }
+
+        .modules-center #window {
+          background-color: @base0B;
+          color: @base00;
+        }
+
+        window#waybar, tooltip, window#waybar.empty #window {
+          background: transparent;
+          color: @base05;
+        }
+
+        .modules-right #clock {
+          background-color: @base09;
+          color: @base00;
+        }
+
+        .modules-left #workspaces button.focused,
+        .modules-left #workspaces button.active {
+          color: @base07;
+        }
+
+        #mpris, #custom-pia {
+          background-color: @base0B;
+          color: @base00;
+        }
+      '';
+
+      settings = {
+        mainBar = {
+          layer = "top";
+          height = 32;
+          spacing = 4;
+          margin = "16px, 16px, 0px, 16px";
+          modules-left = [
+            "niri/workspaces"
+            "mpris"
+          ];
+          modules-center = [
+            "niri/window"
+          ];
+          modules-right = [
+            "mpd"
+            "idle_inhibitor"
+            "pulseaudio"
+            "custom/pia"
+            "network"
+            "power-profiles-daemon"
+            "cpu"
+            "memory"
+            "temperature"
+            "backlight"
+            "battery"
+            "battery#bat2"
+            "clock"
+            "tray"
+            "custom/power"
+          ];
+          idle_inhibitor = {
+            format = "{icon}";
+            format-icons = {
+              activated = "";
+              deactivated = "";
+            };
+          };
+          tray = {
+            spacing = 10;
+          };
+          clock = {
+            tooltip-format = "<big>{:%Y %B}</big>\n<tt><small>{calendar}</small></tt>";
+            format-alt = "{:%Y-%m-%d}";
+          };
+          cpu = {
+            format = "{usage}%  ";
+            tooltip = false;
+          };
+          memory = {
+            format = "{}%  ";
+          };
+          temperature = {
+            critical-threshold = 80;
+            format = "{temperatureC}°C {icon}";
+            format-icons = [
+              ""
+              ""
+              ""
+            ];
+          };
+          backlight = {
+            format = "{percent}% {icon}";
+            format-icons = [
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+              ""
+            ];
+          };
+          battery = lib.mkIf (vars.type == "laptop") {
+            states = {
+              good = 95;
+              warning = 30;
+              critical = 15;
+            };
+            format = "{capacity}% {icon}";
+            format-full = "{capacity}% {icon}";
+            format-charging = "{capacity}% ";
+            format-plugged = "{capacity}% ";
+            format-alt = "{time} {icon}";
+            format-icons = [
+              ""
+              ""
+              ""
+              ""
+              ""
+            ];
+          };
+          power-profiles-daemon = {
+            format = "{icon}";
+            tooltip-format = "Power profile: {profile}\nDriver: {driver}";
+            tooltip = true;
+            format-icons = {
+              default = "";
+              performance = "";
+              balanced = "";
+              power-saver = "";
+            };
+          };
+          "custom/pia" =
+            let
+              monitor = pkgs.writeShellScriptBin "monitor" ''
+                #!${pkgs.stdenv.shell}
+
+                connected="󰌹 "
+                connecting="󱥸 "
+                disconnected="󰌺 "
+
+                if [[ $(pgrep -c "startpia") -gt 0 ]]; then
+                  if grep -q "The port is" /tmp/pia-run.log; then
+                    PORT=$(tail -n 7 /tmp/pia-run.log | grep "Forwarded port" | awk '{print $3}' | tail -n 1)
+                    echo "{\"text\":\"''${connected}''${PORT}\"}"
+                  else
+                    # We are in a connecting state, waiting for the port to be assigned.
+                    echo "{\"text\":\"''${connecting}\"}"
+                  fi
+                else
+                  # Interface does not exist, so we are disconnected.
+                  echo "{\"text\":\"''${disconnected}Connect\"}"
+                fi
+              '';
+              askpass = pkgs.writeShellScriptBin "askpass" ''
+                #!${pkgs.stdenv.shell}
+                ${pkgs.zenity}/bin/zenity --password --title="sudo password"
+              '';
+              toggle = pkgs.writeShellScriptBin "toggle" ''
+                #!${pkgs.stdenv.shell}
+                if [[ $(pgrep -c "startpia") -gt 0 ]]; then
+                  SUDO_ASKPASS="${askpass}/bin/askpass" sudo -A stoppia & disown
+                else
+                  SUDO_ASKPASS="${askpass}/bin/askpass" sudo -A startpia & disown
+                fi
+              '';
+            in
+            {
+              exec = "${monitor}/bin/monitor";
+              return-type = "json";
+              interval = 5;
+              on-click = "${toggle}/bin/toggle";
+              format = " {text} ";
+            };
+          network = {
+            format-wifi = "{essid} ({signalStrength}%)  ";
+            format-ethernet = "{ipaddr}/{cidr}  ";
+            tooltip-format = "{ifname} via {gwaddr}  ";
+            format-linked = "{ifname} (No IP)  ";
+            format-disconnected = "Disconnected ⚠ ";
+            format-alt = "{ifname}: {ipaddr}/{cidr}";
+          };
+          pulseaudio = {
+            tooltip = false;
+            format = "{volume}% {icon}  - {format_source}";
+            format-bluetooth = "{volume}% {icon} - {format_source}";
+            format-bluetooth-muted = "󰝟 {icon} - {format_source}";
+            format-muted = "󰝟  - {format_source}";
+            format-source = "{volume}%  ";
+            format-source-muted = " ";
+            format-icons = {
+              headphone = " ";
+              hands-free = " ";
+              headset = " ";
+              phone = " ";
+              portable = " ";
+              car = " ";
+              default = [
+                ""
+                ""
+                ""
+              ];
+            };
+            on-click = "pgrep pavucontrol > /dev/null && pkill pavucontrol || pavucontrol &";
+          };
+          mpris = {
+            dynamic_order = [
+              "artist"
+              "title"
+              "position"
+              "length"
+            ];
+            format = "{player_icon} ({status_icon}) {dynamic}";
+            player-icons = {
+              spotify = " ";
+              firefox = " ";
+            };
+            status-icons = {
+              paused = "";
+              playing = "";
+            };
+            tooltip-format = "";
+          };
+          "custom/power" =
+            let
+              fuzzel-powerscript = pkgs.writeShellScriptBin "power" ''
+                      SELECTION="$(printf "1 - Lock\n2 - Sleep\n3 - Log out\n4 - Reboot\n5 - Reboot to UEFI\n6 - Hard reboot\n7 - Shutdown" | fuzzel --dmenu -l 7 -p "Power Menu: ")"
+
+                      case $SELECTION in
+                	      *"Lock")
+                					swaylock --screenshots --effect-pixelate 15 --clock;;
+                				*"Sleep")
+                		      systemctl sleep;;
+                		    *"Log out")
+                	  			niri msg action quit;;
+                	  		*"Reboot")
+                	  			systemctl reboot;;
+                	  		*"Reboot to UEFI")
+                	  			systemctl reboot --firmware-setup;;
+                	  		*"Hard reboot")
+                	  			pkexec "echo b > /proc/sysrq-trigger";;
+                	 			*"Shutdown")
+                	  			systemctl poweroff;;
+                      esac
+              '';
+            in
+            {
+              format = "⏻ ";
+              tooltip = false;
+              on-click = "${fuzzel-powerscript}/bin/power";
+              escape = true;
+            };
+        };
+      };
     };
+
+    stylix.targets.waybar.enableCenterBackColors = true;
+    stylix.targets.waybar.enableLeftBackColors = true;
+    stylix.targets.waybar.enableRightBackColors = true;
   };
 }
