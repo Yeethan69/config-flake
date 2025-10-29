cbdb21b317cd02e5acccf0581ba4779b67d886d3
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2025-10-29 12:46:12 +0000

    Niri - Add birdtray.

--- home-manager/users/ethan/modules/DEs/niri.nix
+++ home-manager/users/ethan/modules/DEs/niri.nix
@@ -1,13 +1,46 @@
 {
   lib,
   vars,
+  pkgs,
   ...
 }:
 {
   config = lib.mkIf (vars.DE.name == "niri") {
+    systemd.user.services.birdtray = {
+      Unit = {
+        Description = "Tray application for thunderbird";
+        After = [ "graphical-session.target" ];
+        PartOf = [ "graphical-session.target" ];
+      };
+      Service = {
+        Type = "simple";
+        ExecStart = "${pkgs.birdtray}/bin/birdtray";
+        Restart = "on-failure";
+        RestartSec = "5s";
+        Environment = [
+          "DISPLAY=\":0\""
+        ];
+      };
+      Install = {
+        WantedBy = [ "graphical-session.target" ];
+      };
+    };
+
+    home.file.bird = {
+      target = "Documents/tb.sh";
+      text = ''
+        #! ${pkgs.runtimeShell}
+        export GDK_BACKEND=x11
+        ${pkgs.thunderbird}/bin/thunderbird
+      '';
+      executable = true;
+    };
+
     programs.niri = {
       settings = {
-
+        spawn-at-startup = [
+          { command = [ "systemctl --user restart birdtray.service" ]; }
+        ];
       };
     };
   };
