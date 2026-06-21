3e3db0ff24584608c4acd20faecb3f237c8c03ac
Yeethan69 <46374629+Yeethan69@users.noreply.github.com>
2026-06-21 16:13:30 +0100

    Remove per user flatpak configuration module and update common options

--- home-manager/common-modules/misc/flatpak.nix
+++ home-manager/common-modules/misc/flatpak.nix
@@ -10,5 +10,9 @@
   ];
   config = lib.mkIf (lib.getAttrFromPath [ "flatpak" "enable" ] vars) {
     services.flatpak.enable = true;
+    services.flatpak.packages =
+      if (lib.hasAttr "packages" vars.flatpak) then vars.flatpak.packages else [ ];
+    services.flatpak.overrides =
+      if (lib.hasAttr "overrides" vars.flatpak) then vars.flatpak.overrides else { };
   };
 }
